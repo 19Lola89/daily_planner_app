cd3bc5025167b2cf5c7df5bffd887e3d68184a9e
Lola <86410482+19Lola89@users.noreply.github.com>
2023-02-07 10:44:24 +0000

    layout

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+
+// use moment js to compare current time using moment js to hours in the day. use conditionals to do that. Its to do with red and greeen rows. relates to color. color-code past, present and future. 
+
+// save button save to local storage. 
+//manipulate html directly. 
+//pseudo code : 
+// 1. Create bootstrap elements for calendal elements
+// 2. calendar shows 9am to 5pm hourly text row with save button
+// 3. current hour is highlighted one colour, past- the other and future another (use data-sets? )
+// 4. save button and local storage, input field obv
+// 5. 
